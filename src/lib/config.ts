1161b67ea65826a5c1ec255cd0b529dc1149fd7f
Chris Fredricksen <cmfredricksen@gmail.com>
2023-04-28 13:53:45 -0500

    add lib/configs

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from "$app/environment"
+
+export const title = "Fresh Web Designs"
+export const description = "From the desk of Chris Fredricksen"
+export const url = dev ? "http://localhost/5173" : "url"
